53a4c892d94b6e03af3e952ead15fb0dc3ed210b
CarlosVilela <cvilela@uoc.edu>
2023-11-22 22:51:39 -0500

    Se actualiza

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     mapPk : 'pk.eyJ1IjoiY3ZpbGVsYTE5NzkiLCJhIjoiY2xuYWljM2x2MDJ6bTJzbzF1bzhxOWM1aiJ9.L8Me7xG665eiS43VvjW03A',
-    urlApi : 'http://localhost:4000/localizador',
+    urlApi : 'http://3.134.117.185:4000/localizador',
     styleMapbox : 'mapbox://styles/mapbox/streets-v12',
-    urlSocketIo : 'http://localhost:4000',
+    urlSocketIo : 'http://3.134.117.185:4000',
 };
 
