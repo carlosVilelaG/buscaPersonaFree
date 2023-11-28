33c104302120d77f6680305f81b7c39075193c4f
CarlosVilela <cvilela@uoc.edu>
2023-11-27 22:56:28 -0500

    Se agrega ajustes de configuracion de rutas backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     mapPk : 'pk.eyJ1IjoiY3ZpbGVsYTE5NzkiLCJhIjoiY2xuYWljM2x2MDJ6bTJzbzF1bzhxOWM1aiJ9.L8Me7xG665eiS43VvjW03A',
-    urlApi : 'http://3.134.117.185:4000/localizador',
+    urlApi : 'https://api-back-service-3uomm4d6da-uc.a.run.app/localizador',
     styleMapbox : 'mapbox://styles/mapbox/streets-v12',
-    urlSocketIo : 'http://3.134.117.185:4000',
+    urlSocketIo : 'https://api-back-service-3uomm4d6da-uc.a.run.app',
 };
 
